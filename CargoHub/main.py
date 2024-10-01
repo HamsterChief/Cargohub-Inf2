98e20ba31858313df79edcd9665e935f82a05a54
Jimmy <JimmyJeroense@icloud.com>
2024-10-01 15:09:08 +0200

    update to DB

--- CargoHub/main.py
+++ CargoHub/main.py
@@ -3,55 +3,96 @@ import json
 
 conn = sqlite3.connect('Cargohub.db')
 cursor = conn.cursor()
-with open('data/inventories.json', 'r') as file:
+with open('data/orders.json', 'r') as file:
     clients = json.load(file)
 
 
 # Step 1: Connect to SQLite database
 conn = sqlite3.connect('Cargohub.db')
 cursor = conn.cursor()
+
+
 """
-{"id": 1, 
-"item_id": "P000001", 
-"description": "Face-to-face clear-thinking complexity", 
-"item_reference": "sjQ23408K", 
-"locations": [3211, 24700, 14123, 19538, 31071, 24701, 11606, 11817], 
-"total_on_hand": 262, 
-"total_expected": 0, 
-"total_ordered": 80, 
-"total_allocated": 41, 
-"total_available": 141, 
-"created_at": "2015-02-19 16:08:24", 
-"updated_at": "2015-09-26 06:37:56"}"""
+"id": 1,
+"source_id": 33,
+"order_date": "2019-04-03T11:33:15Z",
+"request_date": "2019-04-07T11:33:15Z",
+"reference": "ORD00001",
+"reference_extra": "Bedreven arm straffen bureau.",
+"order_status": "Delivered",
+"notes": "Voedsel vijf vork heel.",
+"shipping_notes": "Buurman betalen plaats bewolkt.",
+"picking_notes": "Ademen fijn volgorde scherp aardappel op leren.",
+"warehouse_id": 18,
+"ship_to": null,
+"bill_to": null,
+"shipment_id": 1,
+"total_amount": 9905.13,
+"total_discount": 150.77,
+"total_tax": 372.72,
+"total_surcharge": 77.6,
+"created_at": "2019-04-03T11:33:15Z",
+"updated_at": "2019-04-05T07:33:15Z",
+"items": [
+            {
+                "item_id": "P007435",
+                "amount": 23
+            }
+        ]
+"""
 
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS inventories (
+CREATE TABLE IF NOT EXISTS orders (
     id INTEGER PRIMARY KEY,
-    item_id INTEGER NOT NULL,
-    description TEXT NOT NULL,
-    item_reference TEXT,
-    locations TEXT,
-    total_on_hand INTEGER,
-    total_expected INTEGER,
-    total_ordered INTEGER,
-    total_allocated INTEGER,
-    total_available INTEGER,
+    source_id INTEGER,
+    order_date TEXT,
+    request_date TEXT,
+    reference TEXT,
+    reference_extra TEXT,
+    order_status TEXT,
+    notes TEXT,
+    shipping_notes TEXT,
+    picking_notes TEXT,
+    warehouse_id INTEGER,
+    ship_to TEXT,
+    bill_to TEXT,
+    shipment_id INTEGER,
+    total_amount REAL,
+    total_discount REAL,
+    total_tax REAL,
+    total_surcharge REAL,
     created_at TEXT,
     updated_at TEXT
+    items TEXT
 )
 ''')
 
 
 for client in clients:
     # Step 2: Insert data into 'items' table
-    cursor.execute('''
-    INSERT INTO inventories (id, item_id, description, item_reference, locations, total_on_hand, 
-                       total_expected, total_ordered, total_allocated, total_available, created_at, updated_at)
-                        VALUES (?,?,?,?,?,?,?,?,?,?,?,?)
-''', (client['id'], client['item_id'],
-      client['description'], client['item_reference'], client['locations'],
-      client['total_on_hand'], client['total_expected'], client['total_ordered'],
-      client['total_allocated'], client['total_available'], client['created_at'], client['updated_at']))
+    joined_items = ' | '.join(client['items'])
+    cursor.execute('''source_id,
+                   order_date,
+    request_date,
+    reference,
+    reference_extra,
+    order_status,
+    notes,
+    shipping_notes,
+    picking_notes,
+    warehouse_id,
+    ship_to,
+    bill_to,
+    shipment_id,
+    total_amount,
+    total_discount,
+    total_tax,
+    total_surcharge,
+    created_at,
+    updated_at
+    items TEXT (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
+''', (client['id'], client['warehouse_id'], client['code'], client['name'], client['created_at'],
+      client['updated_at']))
 
 
 conn.commit()
