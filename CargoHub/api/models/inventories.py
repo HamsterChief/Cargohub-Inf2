65782cea76cf2347d806a1f2059f8231165247c0
Melvernvd <melvernvandijk@outlook.com>
2024-10-22 13:09:21 +0200

    Update To inventories.py and transfers.py

--- CargoHub/api/models/inventories.py
+++ CargoHub/api/models/inventories.py
@@ -7,7 +7,7 @@ INVENTORIES = []
 
 class Inventories(Base):
     def __init__(self, root_path, is_debug=False):
-        self.data_path = root_path + "cargohub.db"
+        self.data_path = root_path + "inventories.json"
         self.load(is_debug)
 
     def get_inventories(self):
@@ -62,31 +62,96 @@ class Inventories(Base):
         if is_debug:
             self.data = INVENTORIES
         else:
-            conn = sqlite3.connect(self.data_path)
-            cursor = conn.cursor()
-            # select everything from each row from the table
-            cursor.execute("SELECT * FROM inventories")
-            self.data = cursor.fetchall()
-            conn.close()
+            f = open(self.data_path, "r")
+            self.data = json.load(f)
+            f.close()
 
     def save(self):
-        conn = sqlite3.connect(self.data_path)
-        try:
-            cursor = conn.cursor()
-            #executemany to avoid for loop
-            #first time using might have small issues
-            #but it is far more efficient
-            cursor.executemany("""
-                UPDATE inventories
-                SET id = ?, item_id = ?, description = ?, item_reference = ?, locations = ?, total_on_hand = ?, total_expected = ?,
-                               total_ordered = ?, total_allocated = ?, total_available = ?, created_at = ?, updated_at = ?
-                WHERE id = ?
-            """, self.data)
-        except Exception as e:
-            # Rollback so that data doesn't get corrupted
-            conn.rollback()
-            raise e  # Re raise or handle the error
-        finally:
-            # Final statement
-            conn.close()
-        return
+        f = open(self.data_path, "w")
+        json.dump(self.data, f)
+        f.close()
+
+    # def __init__(self, root_path, is_debug=False):
+    #     self.data_path = root_path + "cargohub.db"
+    #     self.load(is_debug)
+
+    # def get_inventories(self):
+    #     return self.data
+
+    # def get_inventory(self, inventory_id):
+    #     for x in self.data:
+    #         if x["id"] == inventory_id:
+    #             return x
+    #     return None
+
+    # def get_inventories_for_item(self, item_id):
+    #     result = []
+    #     for x in self.data:
+    #         if x["item_id"] == item_id:
+    #             result.append(x)
+    #     return result
+
+    # def get_inventory_totals_for_item(self, item_id):
+    #     result = {
+    #         "total_expected": 0,
+    #         "total_ordered": 0,
+    #         "total_allocated": 0,
+    #         "total_available": 0
+    #     }
+    #     for x in self.data:
+    #         if x["item_id"] == item_id:
+    #             result["total_expected"] += x["total_expected"]
+    #             result["total_ordered"] += x["total_ordered"]
+    #             result["total_allocated"] += x["total_allocated"]
+    #             result["total_available"] += x["total_available"]
+    #     return result
+
+    # def add_inventory(self, inventory):
+    #     inventory["created_at"] = self.get_timestamp()
+    #     inventory["updated_at"] = self.get_timestamp()
+    #     self.data.append(inventory)
+
+    # def update_inventory(self, inventory_id, inventory):
+    #     inventory["updated_at"] = self.get_timestamp()
+    #     for i in range(len(self.data)):
+    #         if self.data[i]["id"] == inventory_id:
+    #             self.data[i] = inventory
+    #             break
+
+    # def remove_inventory(self, inventory_id):
+    #     for x in self.data:
+    #         if x["id"] == inventory_id:
+    #             self.data.remove(x)
+
+    # def load(self, is_debug):
+    #     if is_debug:
+    #         self.data = INVENTORIES
+    #     else:
+    #         conn = sqlite3.connect(self.data_path)
+    #         cursor = conn.cursor()
+    #         # select everything from each row from the table
+    #         cursor.execute("SELECT * FROM inventories")
+    #         self.data = cursor.fetchall()
+    #         conn.close()
+
+    # def save(self):
+    #     conn = sqlite3.connect(self.data_path)
+    #     try:
+    #         cursor = conn.cursor()
+    #         #executemany to avoid for loop
+    #         #first time using might have small issues
+    #         #but it is far more efficient
+    #         cursor.executemany("""
+    #             UPDATE inventories
+    #             SET id = ?, item_id = ?, description = ?, item_reference = ?, locations = ?, total_on_hand = ?, total_expected = ?,
+    #                            total_ordered = ?, total_allocated = ?, total_available = ?, created_at = ?, updated_at = ?
+    #             WHERE id = ?
+    #         """, self.data)
+    #     except Exception as e:
+    #         # Rollback so that data doesn't get corrupted
+    #         conn.rollback()
+    #         raise e  # Re raise or handle the error
+    #     finally:
+    #         # Final statement
+    #         conn.close()
+    #     return
--- CargoHub/api/models/transfers.py
+++ CargoHub/api/models/transfers.py
@@ -8,7 +8,7 @@ TRANSFERS = []
 
 class Transfers(Base):
     def __init__(self, root_path, is_debug=False):
-        self.data_path = root_path + "cargohub.db"
+        self.data_path = root_path + "transfers.json"
         self.load(is_debug)
 
     def get_transfers(self):
@@ -27,6 +27,7 @@ class Transfers(Base):
         return None
 
     def add_transfer(self, transfer):
+        transfer["transfer_status"] = "Scheduled"
         transfer["created_at"] = self.get_timestamp()
         transfer["updated_at"] = self.get_timestamp()
         self.data.append(transfer)
@@ -47,31 +48,80 @@ class Transfers(Base):
         if is_debug:
             self.data = TRANSFERS
         else:
-            conn = sqlite3.connect(self.data_path)
-            cursor = conn.cursor()
-            # select everything from each row from the table
-            cursor.execute("SELECT * FROM transfers")
-            self.data = cursor.fetchall()
-            conn.close()
+            f = open(self.data_path, "r")
+            self.data = json.load(f)
+            f.close()
 
     def save(self):
-        conn = sqlite3.connect(self.data_path)
-        try:
-            cursor = conn.cursor()
-            #executemany to avoid for loop
-            #first time using might have small issues
-            #but it is far more efficient
-            cursor.executemany("""
-                UPDATE transfers
-                SET id = ?, reference = ?, transfer_from = ?, transfer_to = ?, transfer_status = ?,
-                               created_at = ?, updated_at = ?, items = ?
-                WHERE id = ?
-            """, self.data)
-        except Exception as e:
-            # Rollback so that data doesn't get corrupted
-            conn.rollback()
-            raise e  # Re raise or handle the error
-        finally:
-            # Final statement
-            conn.close()
-        return
+        f = open(self.data_path, "w")
+        json.dump(self.data, f)
+        f.close()
+
+    # def __init__(self, root_path, is_debug=False):
+    #     self.data_path = root_path + "cargohub.db"
+    #     self.load(is_debug)
+
+    # def get_transfers(self):
+    #     return self.data
+
+    # def get_transfer(self, transfer_id):
+    #     for x in self.data:
+    #         if x["id"] == transfer_id:
+    #             return x
+    #     return None
+
+    # def get_items_in_transfer(self, transfer_id):
+    #     for x in self.data:
+    #         if x["id"] == transfer_id:
+    #             return x["items"]
+    #     return None
+
+    # def add_transfer(self, transfer):
+    #     transfer["created_at"] = self.get_timestamp()
+    #     transfer["updated_at"] = self.get_timestamp()
+    #     self.data.append(transfer)
+
+    # def update_transfer(self, transfer_id, transfer):
+    #     transfer["updated_at"] = self.get_timestamp()
+    #     for i in range(len(self.data)):
+    #         if self.data[i]["id"] == transfer_id:
+    #             self.data[i] = transfer
+    #             break
+
+    # def remove_transfer(self, transfer_id):
+    #     for x in self.data:
+    #         if x["id"] == transfer_id:
+    #             self.data.remove(x)
+
+    # def load(self, is_debug):
+    #     if is_debug:
+    #         self.data = TRANSFERS
+    #     else:
+    #         conn = sqlite3.connect(self.data_path)
+    #         cursor = conn.cursor()
+    #         # select everything from each row from the table
+    #         cursor.execute("SELECT * FROM transfers")
+    #         self.data = cursor.fetchall()
+    #         conn.close()
+
+    # def save(self):
+    #     conn = sqlite3.connect(self.data_path)
+    #     try:
+    #         cursor = conn.cursor()
+    #         #executemany to avoid for loop
+    #         #first time using might have small issues
+    #         #but it is far more efficient
+    #         cursor.executemany("""
+    #             UPDATE transfers
+    #             SET id = ?, reference = ?, transfer_from = ?, transfer_to = ?, transfer_status = ?,
+    #                            created_at = ?, updated_at = ?, items = ?
+    #             WHERE id = ?
+    #         """, self.data)
+    #     except Exception as e:
+    #         # Rollback so that data doesn't get corrupted
+    #         conn.rollback()
+    #         raise e  # Re raise or handle the error
+    #     finally:
+    #         # Final statement
+    #         conn.close()
+    #     return
