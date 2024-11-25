368f375832181f11c21478275bbc37c51ee6f5c5
HamsterChief <144031484+HamsterChief@users.noreply.github.com>
2024-11-25 01:05:37 +0100

    all Endpoints functional

--- MyEFCoreProject/controllers/TransferController.cs
+++ MyEFCoreProject/controllers/TransferController.cs
@@ -51,9 +51,10 @@ public class TransferController : Controller
         return BadRequest("Failed to update transfer.");
     }
 
-    [HttpDelete("transfers/{id}")]
+    [HttpDelete("transfers/{transfer_id}")]
     public async Task<IActionResult> DeleteTransfer(int transfer_id)
     {
+        Console.WriteLine(transfer_id);
         var result = await _transferService.DeleteTransfer(transfer_id);
         if (result)
         {
