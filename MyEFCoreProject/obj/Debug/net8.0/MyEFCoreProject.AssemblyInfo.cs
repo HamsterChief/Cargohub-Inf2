1cbd91926d4d9fc353e50730c8776f977a45e59e
Jimmy <JimmyJeroense@icloud.com>
2024-11-26 10:15:28 +0100

    update to database for API_Keys

--- MyEFCoreProject/obj/Debug/net8.0/MyEFCoreProject.AssemblyInfo.cs
+++ MyEFCoreProject/obj/Debug/net8.0/MyEFCoreProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyEFCoreProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4552ac2fea352b91050cd6a5c852a04142419faf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6b5f960f1385eddf48c4a836daa37b8ec944aa3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyEFCoreProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyEFCoreProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
