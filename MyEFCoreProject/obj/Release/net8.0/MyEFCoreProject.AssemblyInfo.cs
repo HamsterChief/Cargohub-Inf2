622d16c1f74664a83a7e50f7e309a77124531ebe
Jimmy <JimmyJeroense@icloud.com>
2024-12-03 10:14:10 +0100

    test

--- MyEFCoreProject/obj/Release/net8.0/MyEFCoreProject.AssemblyInfo.cs
+++ MyEFCoreProject/obj/Release/net8.0/MyEFCoreProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyEFCoreProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3527d8381bf05a87980c9c4d1b9a925715632e0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc6761405a690356d0445f2c71966b2880a6143c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyEFCoreProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyEFCoreProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
