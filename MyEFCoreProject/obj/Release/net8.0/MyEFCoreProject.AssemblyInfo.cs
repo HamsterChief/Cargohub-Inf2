5ea0d4e55aace2742872a9f323e62550b7ffbdaa
Jimmy <JimmyJeroense@icloud.com>
2024-12-10 10:35:39 +0100

    implementing test db

--- MyEFCoreProject/obj/Release/net8.0/MyEFCoreProject.AssemblyInfo.cs
+++ MyEFCoreProject/obj/Release/net8.0/MyEFCoreProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyEFCoreProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39623542828346c11a7e1f54e5e01da0a5dfd920")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da0c7c6ad184288b698579019a54f0465237bd71")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyEFCoreProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyEFCoreProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
